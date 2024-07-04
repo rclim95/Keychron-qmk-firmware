274da0608ec7fe53fe607f2234e8b8444d6fcfcb
Richard Lim <hello@rclim95.com>
2024-07-04 00:25:15 -0500

    [K3 Pro] Increase the number of EEPROM space available for macros
    In addition, make Fn+Alt toggle the `MOD_2` layer.

--- keyboards/keychron/k3_pro/ansi/rgb/keymaps/rclim95/keymap.c
+++ keyboards/keychron/k3_pro/ansi/rgb/keymaps/rclim95/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
      KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
      KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS, TG(MOD_1), KC_TRNS,  KC_TRNS,  KC_TRNS),
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                               TG(MOD_2), KC_TRNS, TG(MOD_1), KC_TRNS,  KC_TRNS,  KC_TRNS),
 
 [MOD_1] = LAYOUT_ansi_84(
      KC_TRNS,  KC_F13,   KC_F14,   KC_F15,   KC_F16,   KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_F21,   KC_F22,   KC_F23,   KC_F24,   KC_TRNS,  KC_TRNS,  KC_TRNS,
